9a324efbd848cfc43284cb8bef2a7dd1999992f1
KevinLiuTong <kevinliutong@hotmail.com>
2022-01-21 11:19:15 +0800

    generate rocket and boom

--- src/main/scala/chiselexample.scala
+++ src/main/scala/chiselexample.scala
@@ -1,18 +1,16 @@
 package integration
-import boom.common._
+
 import freechips.rocketchip.config._
 import chisel3.stage._
 import firrtl.options.TargetDirAnnotation
 import firrtl.stage.{CompilerAnnotation, RunFirrtlTransformAnnotation}
 import firrtl.transforms.NoDCEAnnotation
 import firrtl.{EmitAllModulesAnnotation, VerilogCompiler, VerilogEmitter}
-
 import freechips.rocketchip.diplomacy.{LazyModule, ValName}
 import freechips.rocketchip.subsystem._
 import freechips.rocketchip.system._
-
 import chiselexamples._
-
+import boom.common.WithNSmallBooms
 object chiselexampletop extends App {
   (new ChiselStage).run(Seq(
     TargetDirAnnotation("./builds/chiselexamples"),
@@ -20,3 +18,32 @@ object chiselexampletop extends App {
     RunFirrtlTransformAnnotation(new VerilogEmitter)
   ))
 }
+object rocket extends App {
+  val param = new TinyConfig
+  (new chisel3.stage.ChiselStage).run(
+    Seq(
+      TargetDirAnnotation("./builds/rocket"),
+      ChiselGeneratorAnnotation(() => new TestHarness()(param)),
+      RunFirrtlTransformAnnotation(new VerilogEmitter)
+    )
+  )
+}
+object smallboom extends App {
+  val param = new SmallBoomConfig
+  (new chisel3.stage.ChiselStage).run(
+    Seq(
+      TargetDirAnnotation("./builds/rocket"),
+      ChiselGeneratorAnnotation(() => new TestHarness()(param)),
+      RunFirrtlTransformAnnotation(new VerilogEmitter)
+    )
+  )
+}
+class SmallBoomConfig extends Config(
+  new WithNSmallBooms(1) ++
+    new WithCoherentBusTopology ++
+    new WithoutTLMonitors ++
+    new BaseConfig().alter((site, here, up) => {
+      case MemoryBusKey => up(MemoryBusKey).copy(beatBytes = 16)
+      case SystemBusKey => up(SystemBusKey).copy(beatBytes = 16)
+    })
+)
