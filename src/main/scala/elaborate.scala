b6b19f2fdad065d0ed6d911f6263bd48554aef90
KevinLiuTong <kevinliutong@hotmail.com>
2022-03-15 13:17:34 +0800

    add vlsu elaborate

---
+++ src/main/scala/elaborate.scala
@@ -0,0 +1,40 @@
+package integration
+import boom.common.{BoomCoreParams, WithNSmallBooms}
+import chisel3.{Bundle, Module}
+import chisel3.stage.{ChiselGeneratorAnnotation, ChiselStage}
+import firrtl.VerilogEmitter
+import firrtl.options.TargetDirAnnotation
+import firrtl.stage.RunFirrtlTransformAnnotation
+import freechips.rocketchip.config._
+import freechips.rocketchip.devices.tilelink.TLTestRAM
+import freechips.rocketchip.diplomacy.{AddressSet, LazyModule, ValName}
+import freechips.rocketchip.subsystem.{BaseSubsystem, BaseSubsystemModuleImp, InSubsystem, MemoryBusKey, SystemBusKey, TilesLocated, WithCoherentBusTopology, WithInclusiveCache}
+import freechips.rocketchip.system.BaseConfig
+import freechips.rocketchip.tile.{TileKey, TileVisibilityNodeKey}
+import freechips.rocketchip.tilelink.{TLEphemeralNode, TLWidthWidget}
+import vlsu._
+class VLSUBoomConfig extends Config(
+  new WithNSmallBooms(1) ++
+    new WithCoherentBusTopology ++
+    new WithInclusiveCache++
+    new BaseConfig().alter((site, here, up) => {
+      case MemoryBusKey => up(MemoryBusKey).copy(beatBytes = 32, blockBytes = 64)
+      case SystemBusKey => up(SystemBusKey).copy(beatBytes = 32, blockBytes = 64)
+      case TileKey => site(TilesLocated(InSubsystem)).head.tileParams
+      case TileVisibilityNodeKey => TLEphemeralNode()(ValName("master"))
+    })
+)
+
+object vlsutop extends App {
+  val param = new VLSUBoomConfig
+  val gp = VLSUGeneralParameters()
+  (new ChiselStage).run(
+    Seq(
+      TargetDirAnnotation("./builds/vlsu"),
+      ChiselGeneratorAnnotation(() => new VLSUHarness(gp)(param)),
+      RunFirrtlTransformAnnotation(new VerilogEmitter)
+    )
+  )
+}
+
+
